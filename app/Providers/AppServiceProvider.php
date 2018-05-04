be92ccf61594dcc2e3459e855ebaf3f97b9ddbb9
mistcheng <guangchao007@126.com>
2018-05-04 22:24:12 +0800

    upgrade laravel from 5.5 to 5.6

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Blade;
 use Laravel\Dusk\DuskServiceProvider;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
@@ -15,6 +16,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Blade::withoutDoubleEncoding();
         if ($this->app->runningUnitTests()) {
             Schema::defaultStringLength(191);
         }
